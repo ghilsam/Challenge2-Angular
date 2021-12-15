6992600631f4180d139e412045dd393ab1d5137a
aghilas <aghil.naitamer@gmail.com>
2021-12-15 15:03:45 +0100

    Add block

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { HeroesComponent } from './heroes/heroes.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { BlockComponent } from './block/block.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // HeroesComponent,
+    BlockComponent,
+
   ],
   imports: [
     BrowserModule,
+    // FormsModule,
     AppRoutingModule
   ],
   providers: [],
